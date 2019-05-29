a939e31fcdd4b021aa6d91c8d73cd6c4b4b1c63b
xiaolongrenking <1764167990@qq.com>
2019-05-29 17:14:39 +0800

    新增工具类

---
+++ src/main/java/com/offcn/common/Util.java
@@ -0,0 +1,17 @@
+
+package com.offcn.common;
+
+/**
+ * @author Administrator
+ * TODO 更新测试
+ * @date  2019年5月29日
+ */
+public class Util {
+
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("哈哈哈");
+	}
+
+}
